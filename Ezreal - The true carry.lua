ff3cc965bf054555480fba0e17158190439f99bc
linkpad <linkpad@hotmail.com>
2015-01-21 19:09:18 +0100

    Update Ezreal - The true carry.lua

--- Ezreal - The true carry.lua
+++ Ezreal - The true carry.lua
@@ -1,4 +1,4 @@
-local version = "0.2a"
+local version = "0.1"
  
 if myHero.charName ~= "Ezreal" then return end
  
@@ -71,40 +71,26 @@ function OnTick()
 	if ComboKey then
 		Combo(Target)
 	end
+
+	if SxOrb.IsHarass then
+		Harass(Target)
+	end
 	
 	KillSteall()
 	Checks()
 end
 
-function KillSteall()
-	for _, unit in pairs(GetEnemyHeroes()) do
-		local health = unit.health
-		local dmgW = getDmg("W", unit, myHero) + ((myHero.ap)*0.8)
-			if health < dmgW and Settings.killsteal.useW and ValidTarget(unit) then
-				CastW(unit)
-			end
-	 end
-end
-
-function Checks()
-	SkillQ.ready = (myHero:CanUseSpell(_Q) == READY)
-	SkillW.ready = (myHero:CanUseSpell(_W) == READY)
-	-- SkillE.ready = (myHero:CanUseSpell(_E) == READY)
-	SkillR.ready = (myHero:CanUseSpell(_R) == READY)
+function Harass(unit)
+	if ValidTarget(unit) and unit ~= nil and unit.type == myHero.type then
 	
-	TargetSelector:update()
-	Target = GetCustomTarget()
-	SxOrb:ForceTarget(Target)
-	
-	--if VIP_USER and Settings.misc.skinList then ChooseSkin() end
-	if Settings.drawing.lfc.lfc then _G.DrawCircle = DrawCircle2 else _G.DrawCircle = _G.oldDrawCircle end
-end
+		if Settings.harass.useQ then 
+			CastQ(unit)
+		end	
 
-function GetCustomTarget()
- 	TargetSelector:update() 	
-	if _G.MMA_Target and _G.MMA_Target.type == myHero.type then return _G.MMA_Target end
-	if _G.AutoCarry and _G.AutoCarry.Crosshair and _G.AutoCarry.Attack_Crosshair and _G.AutoCarry.Attack_Crosshair.target and _G.AutoCarry.Attack_Crosshair.target.type == myHero.type then return _G.AutoCarry.Attack_Crosshair.target end
-	return TargetSelector.target
+		if Settings.harass.useW then
+			CastW(unit)
+		end
+	end
 end
 
 function Combo(unit)
@@ -124,6 +110,48 @@ function Combo(unit)
 				end
 		end
 	end
+end
+
+function KillSteall()
+	for _, unit in pairs(GetEnemyHeroes()) do
+		local health = unit.health
+		local dmgW = getDmg("W", unit, myHero) + ((myHero.ap)*0.8)
+		local dmgQ = getDmg("Q", unit, myHero) + ((myHero.damage)*1.1) + ((myHero.ap)*0.4)
+			if health < dmgW and Settings.killsteal.useW and ValidTarget(unit) then
+				CastW(unit)
+			end
+			if health < dmgQ and Settings.killsteal.useQ and ValidTarget(unit) then
+				CastQ(unit)
+			end
+	 end
+end
+
+function Checks()
+	SkillQ.ready = (myHero:CanUseSpell(_Q) == READY)
+	SkillW.ready = (myHero:CanUseSpell(_W) == READY)
+	SkillR.ready = (myHero:CanUseSpell(_R) == READY)
+	
+	TargetSelector:update()
+	if SelectedTarget ~= nil and ValidTarget(SelectedTarget) and GetDistance(SelectedTarget) <= SkillQ.range + 500 then
+		Target = SelectedTarget
+	else
+		Target = GetCustomTarget()
+	end
+
+	SxOrb:ForceTarget(Target)
+	
+	if Settings.drawing.lfc.lfc then 
+		_G.DrawCircle = DrawCircle2 
+	else 
+		_G.DrawCircle = _G.oldDrawCircle 
+	end
+end
+
+function GetCustomTarget()
+ 	TargetSelector:update() 	
+	if _G.MMA_Target and _G.MMA_Target.type == myHero.type then return _G.MMA_Target end
+	if _G.AutoCarry and _G.AutoCarry.Crosshair and _G.AutoCarry.Attack_Crosshair and _G.AutoCarry.Attack_Crosshair.target and _G.AutoCarry.Attack_Crosshair.target.type == myHero.type then return _G.AutoCarry.Attack_Crosshair.target end
+	return TargetSelector.target
 end
 
 function CastQ(unit)
@@ -148,7 +176,7 @@ end
 
 function CastR(unit)
 	if unit ~= nil and SkillR.ready then
-		CastPosition,  HitChance,  Position = VP:GetLineCastPosition(unit, SkillR.delay, SkillR.width, SkillR.range, SkillR.speed, myHero, false)
+		CastPosition,  HitChance,  Position = VP:GetLineAOECastPosition(unit, SkillR.delay, SkillR.width, SkillR.range, SkillR.speed, myHero)
 				
 		if HitChance >= 2 then
 			CastSpell(_R, CastPosition.x, CastPosition.z)
@@ -156,12 +184,10 @@ function CastR(unit)
 	end
 end
 
-
 function Skills()
-	SkillQ = { name = "Mystic Shot", range = 1150, delay = 0.1, speed = math.huge, width = 80, ready = false }
-	SkillW = { name = "Essence Flux", range = 950, delay = 0.1, speed = math.huge, width = 80, ready = false }
-	-- SkillE = { name = "Power Fist", range = 250, delay = nil, speed = nil, width = nil, ready = false }
-	SkillR = { name = "Trueshot Barrage", range = nil, delay = 0.5, speed = math.huge, width = 200, ready = false }
+	SkillQ = { name = "Mystic Shot", range = 1150, delay = 0.25, speed = 2000, width = 60, ready = false }
+	SkillW = { name = "Essence Flux", range = 950, delay = 0.25, speed = 1600, width = 80, ready = false }
+	SkillR = { name = "Trueshot Barrage", range = math.huge, delay = 1.0, speed = 2000, width = 160, ready = false }
 	
 	VP = VPrediction()
 
@@ -176,29 +202,35 @@ function Menu()
 	
 	Settings:addSubMenu("["..myHero.charName.."] - Combo Settings", "combo")
 		Settings.combo:addParam("comboKey", "Combo Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
-		Settings.combo:addParam("useQ", "Use (Q) in Combo", SCRIPT_PARAM_ONOFF, false)
-		Settings.combo:addParam("useW", "Use (W) in Combo", SCRIPT_PARAM_ONOFF, false)
-		Settings.combo:addParam("useR", "Use (R) in Combo", SCRIPT_PARAM_ONOFF, false)
+		Settings.combo:addParam("useQ", "Use (Q) in Combo", SCRIPT_PARAM_ONOFF, true)
+		Settings.combo:addParam("useW", "Use (W) in Combo", SCRIPT_PARAM_ONOFF, true)
 		Settings.combo:addParam("RifKilable", "Use (R) if enemy is kilable", SCRIPT_PARAM_ONOFF, true)
 		
-	Settings:addSubMenu("["..myHero.charName.."] - KillSteal", "killsteal")	
-		Settings.killsteal:addParam("useW", "Steal With (W)", SCRIPT_PARAM_ONOFF, false)
+	Settings:addSubMenu("["..myHero.charName.."] - KillSteal", "killsteal")
+		Settings.killsteal:addParam("useQ", "Steal With (Q)", SCRIPT_PARAM_ONOFF, true)
+		Settings.killsteal:addParam("useW", "Steal With (W)", SCRIPT_PARAM_ONOFF, true)
+
+	Settings:addSubMenu("["..myHero.charName.."] - Harass", "harass")
+		Settings.harass:addParam("useQ", "Harass With (Q)", SCRIPT_PARAM_ONOFF, true)
+		Settings.harass:addParam("useW", "Harass With (W)", SCRIPT_PARAM_ONOFF, true)
 		
 	Settings.combo:permaShow("comboKey")
 	Settings.combo:permaShow("useR")
 	Settings.combo:permaShow("RifKilable")
+	Settings.killsteal:permaShow("useQ")
 	Settings.killsteal:permaShow("useW")
 	
 	
 	Settings:addSubMenu("["..myHero.charName.."] - Draw Settings", "drawing")	
 		Settings.drawing:addParam("mDraw", "Disable All Range Draws", SCRIPT_PARAM_ONOFF, false)
 		Settings.drawing:addParam("myHero", "Draw My Range", SCRIPT_PARAM_ONOFF, true)
-		Settings.drawing:addParam("myColor", "Draw My Range Color", SCRIPT_PARAM_COLOR, {0, 100, 44, 255})
+		Settings.drawing:addParam("myColor", "Draw My Range Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
 		Settings.drawing:addParam("qDraw", "Draw "..SkillQ.name.." (Q) Range", SCRIPT_PARAM_ONOFF, true)
-		Settings.drawing:addParam("qColor", "Draw "..SkillQ.name.." (Q) Color", SCRIPT_PARAM_COLOR, {0, 100, 44, 255})
+		Settings.drawing:addParam("qColor", "Draw "..SkillQ.name.." (Q) Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
 		Settings.drawing:addParam("wDraw", "Draw "..SkillW.name.." (W) Range", SCRIPT_PARAM_ONOFF, true)
-		Settings.drawing:addParam("wColor", "Draw "..SkillW.name.." (W) Color", SCRIPT_PARAM_COLOR, {0, 100, 44, 255})
-		
+		Settings.drawing:addParam("wColor", "Draw "..SkillW.name.." (W) Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
+		Settings.drawing:addParam("tColor", "Draw Target Color", SCRIPT_PARAM_COLOR, {255, 100, 44, 255})
+
 	Settings.drawing:addSubMenu("Lag Free Circles", "lfc")	
 		Settings.drawing.lfc:addParam("lfc", "Lag Free Circles", SCRIPT_PARAM_ONOFF, false)
 		Settings.drawing.lfc:addParam("CL", "Quality", 4, 75, 75, 2000, 0)
@@ -206,7 +238,7 @@ function Menu()
 	Settings:addSubMenu("["..myHero.charName.."] - Orbwalking Settings", "Orbwalking")
 		SxOrb:LoadToMenu(Settings.Orbwalking)
 	
-	TargetSelector = TargetSelector(TARGET_LESS_CAST_PRIORITY, SkillQ.range, DAMAGE_MAGIC, true)
+	TargetSelector = TargetSelector(TARGET_LESS_CAST_PRIORITY, SkillQ.range + 500, DAMAGE_PHYSICAL, false, true)
 	TargetSelector.name = "Ezreal"
 	Settings:addTS(TargetSelector)
 end
@@ -221,9 +253,57 @@ function OnDraw()
 		end
 		
 		if Settings.drawing.myHero then
-			DrawCircle(myHero.x, myHero.y, myHero.z, myHero.range, RGB(Settings.drawing.myColor[2], Settings.drawing.myColor[3], Settings.drawing.myColor[4]))
+			
+			DrawCircle(myHero.x, myHero.y, myHero.z, myHero.range + GetDistance(myHero, myHero.minBBox), RGB(Settings.drawing.myColor[2], Settings.drawing.myColor[3], Settings.drawing.myColor[4]))
+		end
+
+		if Target ~= nil then
+			DrawCircle(Target.x, Target.y, Target.z, 200, RGB(Settings.drawing.tColor[2], Settings.drawing.tColor[3], Settings.drawing.tColor[4]))
 		end
 	end
+end
+
+function OnWndMsg(Msg, Key)
+	if Msg == WM_LBUTTONDOWN then
+		local minD = 0
+		local starget = nil
+		for i, enemy in ipairs(GetEnemyHeroes()) do
+			if ValidTarget(enemy) then
+				if GetDistance(enemy, mousePos) <= minD or starget == nil then
+					minD = GetDistance(enemy, mousePos)
+					starget = enemy
+				end
+			end
+		end
+		
+		if starget and minD < 500 then
+			if SelectedTarget and starget.charName == SelectedTarget.charName then
+				SelectedTarget = nil
+			else
+				SelectedTarget = starget
+				SxOrb:ForceTarget(SelectedTarget)
+			end
+		end
+	end
+end
+
+function DrawCircleNextLvl(x, y, z, radius, width, color, chordlength)
+  radius = radius or 300
+  quality = math.max(8,round(180/math.deg((math.asin((chordlength/(2*radius)))))))
+  quality = 2 * math.pi / quality
+  radius = radius*.92
+  
+  local points = {}
+  for theta = 0, 2 * math.pi + quality, quality do
+    local c = WorldToScreen(D3DXVECTOR3(x + radius * math.cos(theta), y, z - radius * math.sin(theta)))
+    points[#points + 1] = D3DXVECTOR2(c.x, c.y)
+  end
+  
+  DrawLines2(points, width or 1, color or 4294967295)
+end
+
+function round(num) 
+  if num >= 0 then return math.floor(num+.5) else return math.ceil(num-.5) end
 end
 
 function DrawCircle2(x, y, z, radius, color)
@@ -231,4 +311,8 @@ function DrawCircle2(x, y, z, radius, color)
   local vPos2 = Vector(cameraPos.x, cameraPos.y, cameraPos.z)
   local tPos = vPos1 - (vPos1 - vPos2):normalized() * radius
   local sPos = WorldToScreen(D3DXVECTOR3(tPos.x, tPos.y, tPos.z))
+
+  if OnScreen({ x = sPos.x, y = sPos.y }, { x = sPos.x, y = sPos.y }) then
+    DrawCircleNextLvl(x, y, z, radius, Settings.drawing.lfc.Width, color, Settings.drawing.lfc.CL) 
+  end
 end
